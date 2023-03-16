ab0d76e4a46a0eac5553bca6d5dea5a137594b34
VasiliyZaharov <zaharovfreelance@gmail.com>
2023-03-16 21:03:06 +0300

    module e2. introduction to javascript

---
+++ Module E2. Introduction to JavaScript/task_3.js
@@ -0,0 +1,6 @@
+const string = 'Hello';
+
+// split разбивает строку на массив символов
+// меняет местами символы (элементы массива)
+// соединяет массив в строку
+const reverseString = string.split("").reverse().join("");
---
+++ Module E2. Introduction to JavaScript/task_5.js
@@ -0,0 +1,6 @@
+const arr = [0, null, NaN, 0, 1, 2, 4, "str"]
+console.log('количество элементов: ', arr.length)
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+     }
